5e1fc0ebb161267bb402d674f4a6e1f63b40c627
0Romero <romerocontato08@gmail.com>
2025-11-20 16:05:11 -0300

    change variable names

--- src/components/SearchAndFilters.jsx
+++ src/components/SearchAndFilters.jsx
@@ -36,9 +36,9 @@ export function SearchAndFilters({
             className="w-full rounded-lg border border-slate-300 dark:border-slate-700 bg-white dark:bg-slate-900 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary/60"
           >
             <option value="">Todas</option>
-            {areas.map((a) => (
-              <option key={a} value={a}>
-                {a}
+            {areas.map((area) => (
+              <option key={area} value={area}>
+                {area}
               </option>
             ))}
           </select>
